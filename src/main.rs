052fce2f3bca6bd4cf4601bdbb12c34d6ee1a386
Pascal Niggemeier <pascal.niggemeier@fhdw.de>
2022-07-09 15:46:43 +0200

    Quitting After a Correct Guess

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line.");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a nubmer!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
